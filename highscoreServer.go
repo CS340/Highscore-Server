3edbf56eb647aefcc84904de00652ab99f6a3b37
Anthony Phelps <anthonypphelps@gmail.com>
2012-10-26 17:54:38 -0500

    Almost works. Added in the mysql stuff just need it to return the right thing

--- highscoreServer.go
+++ highscoreServer.go
@@ -4,9 +4,9 @@ import (
 	"net"
 	"os"
 	"fmt"
-	//"io/ioutil"
+	"strings"
+	"github.com/thoj/Go-MySQL-Client-Library"
 	"time"
-	//"strconv"
 )
 
 func main() {
@@ -22,39 +22,85 @@ func main() {
 		if(err != nil){
 			continue
 		}
+		logIt("CONNECTION", "Got new connection")
 		
-		//connection.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
-		newClient(connection)
-		connection.Close()
+		go newClient(connection)
+		
 	}
 
 	os.Exit(0)
 }
 
 func newClient(connect net.Conn){
-
+	logIt("CONNECTION", "Handling new client")
 	var buffer [512]byte
 
-	for{
-		n, err := connect.Read(buffer[0:])
-		if err != nil {
-			return
-		}
-
-		fmt.Println(string(buffer[0:]))
-		_, err2 := connect.Write(buffer[0:n])
-		if err2 != nil {
-			return
-		}
+	_, err := connect.Read(buffer[0:])
+	if err != nil {
+		logError("ERROR", "Error reading from client", err)
+		connect.Close()
+		return
 	}
+
+	commm, _ := parseCommand(string(buffer[0:]))
+	_, err2 := connect.Write([]byte(commm))
+	if err2 != nil {
+		logError("ERROR", "Error writing to client", err2)
+		connect.Close()
+		return
+	}
+	connect.Close()
+	logIt("CONNECTION", "Closing connection to client")
 }
 
-func logIt(ertype string, message string, err error){
-	fmt.Printf("%s\t%s: %s: %s", time.Now().String(), ertype, message, err)
+func logError(ertype string, message string, err error){
+	fmt.Printf("%s\t%s: %s: %s\n", time.Now().String(), ertype, message, err)
+}
+
+func logIt(ertype string, message string){
+	fmt.Printf("%s\t%s: %s\n", time.Now().String(), ertype, message)
 }
 
 func errorCheck(err error, message string){
 	if(err != nil){
-		logIt("ERROR", message, err)
+		logError("ERROR", message, err)
 	}
+}
+
+func parseCommand(com string) (string, int){
+
+	dataCon, err := mysql.Connect("tcp", "127.0.0.1:3306", "highscores", "hhss", "highscores")
+	errorCheck(err, "Could not connect to MySQL database.")
+	var out int
+	out = 1
+	
+	scores := new(mysql.MySQLResponse)
+
+	parts := strings.Split(com, ":")
+
+	switch parts[0]{
+		case "user": 
+			switch parts[1]{
+				case "new":
+					logIt("MySQL:Query", "Inserting new user " + parts[2] + " " + parts[3] + ", " + parts[4])
+					_, err = dataCon.Query("INSERT INTO users (firstName,lastName,username) VALUES('" + parts[2] + "', " + parts[3] + "', " + parts[4] + ")")
+					errorCheck(err, "Could not enter new user into database. USER: " + parts[2])
+			}
+		case "score":
+			switch parts[1]{
+				case "new":
+					logIt("MySQL:Query", "Inserting new score of " + parts[3] + " for " + parts[2])
+					_, err = dataCon.Query("INSERT INTO scores (username,score) VALUES('" + parts[2] + "', " + parts[3] + ")")
+					errorCheck(err, "Could not enter score into database. USER: " + parts[2] + "SCORE:" + parts[3])
+				case "get":
+					logIt("MySQL:Query", "Reading scores for " + parts[2])
+					fmt.Println("SELECT * FROM scores WHERE `username` = 'aphelps' ORDER BY score DESC")
+					scores, err = dataCon.Query("SELECT * FROM scores WHERE username = " + parts[2] + " ORDER BY score DESC")
+					errorCheck(err, "Could not get scores from database. USER: " + parts[2])
+					fmt.Println(scores.FetchRowMap())
+			}
+	}
+	dataCon.Quit();
+
+	return com, out
 }
